5f2cfe930671892a1c77d8244df1949974369b02
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-03-26 13:38:03 -0400

    Update app.js
    more specific selector for the navigation menu, using event delegation for the menu item clicks, and wrapping the entire script in an immediately-invoked function expression (IIFE) to prevent global scope pollution.

--- .github/Website/app.js
+++ .github/Website/app.js
@@ -1,29 +1,30 @@
-// get the navigation menu and its list items
-const menu = document.querySelector('nav ul');
-const menuItems = menu.querySelectorAll('li a');
+(function() {
+  // Get the navigation menu and its list items
+  const menu = document.querySelector('nav .nav-menu ul');
 
-// add an event listener to each menu item
-menuItems.forEach(item => {
-  item.addEventListener('click', function(e) {
-    // prevent the default behavior of the link
+  // Add an event listener to the navigation menu using event delegation
+  menu.addEventListener('click', function(e) {
+    const target = e.target;
+
+    // Check if the clicked element is an anchor inside the menu
+    if (target.tagName === 'A' && target.closest('nav ul')) {
+      e.preventDefault();
+
+      const section = document.querySelector(target.getAttribute('href'));
+
+      section.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+
+  // Get the "Shop Now" button and add an event listener to it
+  const shopNowBtn = document.querySelector('.btn');
+  shopNowBtn.addEventListener('click', function(e) {
+    // Prevent the default behavior of the button
     e.preventDefault();
 
-    // get the section that corresponds to the clicked menu item
-    const section = document.querySelector(this.getAttribute('href'));
-
-    // scroll to the section smoothly
-    section.scrollIntoView({
-      behavior: 'smooth'
-    });
+    // Alert the user that the shop is not yet available
+    alert('The shop is not yet available.');
   });
-});
-
-// get the "Shop Now" button and add an event listener to it
-const shopNowBtn = document.querySelector('.btn');
-shopNowBtn.addEventListener('click', function(e) {
-  // prevent the default behavior of the button
-  e.preventDefault();
-
-  // alert the user that the shop is not yet available
-  alert('The shop is not yet available.');
-});
+})();
