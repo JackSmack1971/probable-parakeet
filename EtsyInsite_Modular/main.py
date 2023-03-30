9483ef264be028c6714931d363a001b780203545
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-03-30 14:11:39 -0400

    Update main.py
    Added google trends data using their API and added parsing to the spreadsheet.

--- EtsyInsite_Modular/main.py
+++ EtsyInsite_Modular/main.py
@@ -1,3 +1,4 @@
+from pytrends.request import TrendReq
 import logging
 import argparse
 from dotenv import load_dotenv
@@ -33,27 +34,46 @@ def read_config_file(config_file):
         raise ValueError("Config file must have [ETSY] section with API_KEY and SHOP_ID options")
     if not config.has_section(SHEET_SECTION) or not config.has_option(SHEET_SECTION, SPREADSHEET_ID) or not config.has_option(SHEET_SECTION, SERVICE_ACCOUNT_FILE):
         raise ValueError("Config file must have [GOOGLE_SHEET] section with SPREADSHEET_ID and SERVICE_ACCOUNT_FILE options")
-    return config
+    return config 
 
+def get_trends_data(keyword):
+    # Authenticate and connect to the Google Trends API
+    pytrends = TrendReq() 
+
+    # Set the date range
+    date_range = 'today 5-y'  # 5 years of daily data 
+
+    # Build the payload and retrieve the data
+    pytrends.build_payload(kw_list=[keyword], timeframe=date_range)
+    trends_data = pytrends.interest_over_time() 
+
+    # Rename the column to the keyword for consistency
+    trends_data.rename(columns={keyword: 'trends_data'}, inplace=True) 
+
+    return trends_data 
 
 def main(config):
+    # Retrieve the API key, shop ID, spreadsheet ID, and service account file from the config file
     api_key = config.get(API_SECTION, ETSY_API_KEY)
     shop_id = config.get(API_SECTION, SHOP_ID)
     spreadsheet_id = config.get(SHEET_SECTION, SPREADSHEET_ID)
     service_account_file = config.get(SHEET_SECTION, SERVICE_ACCOUNT_FILE) 
 
-    if not api_key or not shop_id or not spreadsheet_id or not service_account_file:
-        raise ValueError("Missing required values in config file") 
-
+    # Initialize the API wrapper, data processor, and sheets integration objects
     etsy_api = EtsyApiWrapper(api_key)
-    data_processor = DataProcessing(etsy_api) 
+    data_processor = DataProcessing(etsy_api)
+    sheets_integration = GoogleSheetsIntegration(spreadsheet_id, service_account_file) 
 
+    # Retrieve the processed data from the Etsy API and update with Google Trends data
     processed_data = data_processor.process_listings(shop_id)
-    logger.info("Processed %d listings", len(processed_data)) 
+    for i in range(len(processed_data)):
+        listing_title = processed_data[i]['title']
+        trends_data = get_trends_data(listing_title)
+        if not trends_data.empty:
+            processed_data[i]['trends_data'] = trends_data['trends_data'].values[0] 
 
-    sheets_integration = GoogleSheetsIntegration(spreadsheet_id, service_account_file)
+    # Update the Google Sheets with the processed data
     sheets_integration.update_sheet(processed_data)
-    logger.info("Updated Google Sheets with %d rows", len(processed_data))
 
 
 if __name__ == "__main__":
