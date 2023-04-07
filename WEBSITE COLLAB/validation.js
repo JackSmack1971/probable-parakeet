445b815cf3758addbab0ca7c5680afe6b89c29dc
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-04-06 23:00:23 -0400

    Create validation.js
    V1

---
+++ WEBSITE COLLAB/validation.js
@@ -0,0 +1,14 @@
+function isValidName(name) {
+    // Add your validation logic for names
+    return true;
+} 
+
+function isValidEmail(email) {
+    // Add your validation logic for emails
+    return true;
+} 
+
+function isValidProof(proof) {
+    // Add your validation logic for proofs
+    return true;
+}
