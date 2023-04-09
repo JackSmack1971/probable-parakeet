fd690d4d70894652cf841789233cb9e4259a66a4
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-04-09 13:31:57 -0400

    Update Unintests.py

--- FlashbotV3/Unintests.py
+++ FlashbotV3/Unintests.py
@@ -1,8 +1,11 @@
-def test_calculate_gas_price():
-    # Create a mock transaction with a known value and gas limit
+import pytest
+from unittest import mock
+from frontrunning_bot import calculate_gas_price, create_new_tx, broadcast_tx
+
+def test_calculate_gas_price(w3, contract):
     tx = {
-        'from': '0x1234...',
-        'to': CONTRACT_ADDRESS,
+        'from_address': '0x1234...',
+        'to': contract.address,
         'value': 1000000000000000000,
         'gas': 100000,
         'gasPrice': 20000000000,
@@ -10,26 +13,22 @@ def test_calculate_gas_price():
         'input': contract.functions.transfer('0x5678...', 100000000000000000).buildTransaction({'from': '0x1234...'})['data']
     }
 
-    # Mock the calculate_expected_profit function
     def mock_calculate_expected_profit(w3, tx, gas_price):
-        return tx.value - gas_price * tx.gas
+        return tx['value'] - gas_price * tx['gas']
 
-    # Test the function with a profitable gas price
-    with mock.patch('frontrun.calculate_expected_profit', side_effect=mock_calculate_expected_profit):
+    with mock.patch('frontrunning_bot.calculate_expected_profit', side_effect=mock_calculate_expected_profit):
         gas_price = calculate_gas_price(w3, tx)
-        assert gas_price > tx.gasPrice
+        assert gas_price > tx['gasPrice']
 
-    # Test the function with an unprofitable gas price
     tx['value'] = 10000000000000000  # 0.01 ETH
-    with mock.patch('frontrun.calculate_expected_profit', side_effect=mock_calculate_expected_profit):
+    with mock.patch('frontrunning_bot.calculate_expected_profit', side_effect=mock_calculate_expected_profit):
         with pytest.raises(ValueError):
             calculate_gas_price(w3, tx)
 
-def test_create_new_tx():
-    # Create a mock transaction with a known gas price and nonce
+def test_create_new_tx(w3, contract, YOUR_PRIVATE_KEY):
     tx = {
         'from': '0x1234...',
-        'to': CONTRACT_ADDRESS,
+        'to': contract.address,
         'value': 1000000000000000000,
         'gas': 100000,
         'gasPrice': 20000000000,
@@ -37,18 +36,15 @@ def test_create_new_tx():
         'input': contract.functions.transfer('0x5678...', 100000000000000000).buildTransaction({'from': '0x1234...'})['data']
     }
 
-    # Mock the encode_abi function
     def mock_encode_abi(types, values):
         return b'\x00\x01'
 
-    # Test the function with a known gas price and nonce
     with mock.patch('eth_abi.encode_abi', side_effect=mock_encode_abi):
-        new_tx = create_new_tx(w3, tx, 22000000000)
+        new_tx = create_new_tx(w3, tx, 22000000000, YOUR_PRIVATE_KEY)
         assert new_tx['gasPrice'] == 22000000000
         assert new_tx['nonce'] == 0
 
-def test_broadcast_tx():
-    # Create a mock transaction with a known hash
+def test_broadcast_tx(w3):
     new_tx = w3.eth.account.sign_transaction({
         'from': '0x1234...',
         'to': CONTRACT_ADDRESS,
@@ -57,13 +53,12 @@ def test_broadcast_tx():
         'gasPrice': 22000000000,
         'nonce': 0,
         'data': b'\x00\x01',
-    }, private_key='YOUR_PRIVATE_KEY')
+    }, private_key=YOUR_PRIVATE_KEY)
 
-    # Mock the send_raw_transaction function
     def mock_send_raw_transaction(raw_transaction):
         return b'\x00' * 32
 
-    # Test the function with a known transaction hash
     with mock.patch('web3.eth.Eth.send_raw_transaction', side_effect=mock_send_raw_transaction):
-        broadcast_tx(w3, new_tx)
-        assert logging.getLogger().info.called_with('Broadcasting transaction: %s', '0x' + '00' * 32)
+        with mock.patch('logging.info') as log_info_mock:
+            broadcast_tx(w3, new_tx)
+            log_info_mock.assert_called_with('Broadcasting transaction: %s', '0x' + '00' * 32)
