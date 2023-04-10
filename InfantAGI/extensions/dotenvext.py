894b7b920bf84b6dd7aa42e4dddd18659c6e9066
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-04-09 23:35:27 -0400

    Update dotenvext.py
    V1.1.0

--- InfantAGI/extensions/dotenvext.py
+++ InfantAGI/extensions/dotenvext.py
@@ -1,10 +1,18 @@
-from dotenv import load_dotenv
+#dotenvext.py v1.1.0 
+
+from dotenv import load_dotenv 
 
 def load_dotenv_extensions(dotenv_files):
-    for dotenv_file in dotenv_files:
-        try:
-            load_dotenv(dotenv_file)
-        except IOError as e:
-            print(f"Unable to load {dotenv_file}: {e}")
-        except Exception as e:
-            print(f"An error occurred while loading {dotenv_file}: {e}")
+    """
+    Load environment variables from the provided dotenv files and handle exceptions. 
+
+    Args:
+        dotenv_files (list): A list of dotenv files to load.
+    """
+    for dotenv_file in dotenv_files:
+        try:
+            load_dotenv(dotenv_file)
+        except IOError as e:
+            print(f"Unable to load dotenv file '{dotenv_file}': {e}")
+        except Exception as e:
+            print(f"An error occurred while loading dotenv file '{dotenv_file}': {e}")
