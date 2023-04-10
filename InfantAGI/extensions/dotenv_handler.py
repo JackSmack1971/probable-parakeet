28eba9f8910b53b9f321a9d00d1b4c96a76cae7d
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-04-09 23:34:29 -0400

    Update dotenv_handler.py
    V1.1.0

--- InfantAGI/extensions/dotenv_handler.py
+++ InfantAGI/extensions/dotenv_handler.py
@@ -1,5 +1,13 @@
-from dotenv import load_dotenv
+#dotenv_handler.py v1.1.0 
+
+from dotenv import load_dotenv 
 
 def load_dotenv_extensions(dotenv_files):
-    for dotenv_file in dotenv_files:
-        load_dotenv(dotenv_file)
+    """
+    Load environment variables from the provided dotenv files. 
+
+    Args:
+        dotenv_files (list): A list of dotenv files to load.
+    """
+    for dotenv_file in dotenv_files:
+        load_dotenv(dotenv_file)
