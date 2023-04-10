be6d863115c410a71d253b6762a0c17ebacb71eb
JackSmack1971 <128181597+JackSmack1971@users.noreply.github.com>
2023-04-09 23:36:58 -0400

    Update config.py
    V1.1.0

--- InfantAGI/config.py
+++ InfantAGI/config.py
@@ -1,15 +1,20 @@
-import os
+#config.py v1.1.0 
 
-# ANSI escape codes
+import os 
+
+# ANSI escape codes for displaying colored headers and footers
 TASK_LIST_HEADER = os.environ.get("TASK_LIST_HEADER", "\033[95m\033[1m")
 TASK_LIST_FOOTER = os.environ.get("TASK_LIST_FOOTER", "\033[0m\033[0m")
 NEXT_TASK_HEADER = os.environ.get("NEXT_TASK_HEADER", "\033[92m\033[1m")
 NEXT_TASK_FOOTER = os.environ.get("NEXT_TASK_FOOTER", "\033[0m\033[0m")
 TASK_RESULT_HEADER = os.environ.get("TASK_RESULT_HEADER", "\033[93m\033[1m")
-TASK_RESULT_FOOTER = os.environ.get("TASK_RESULT_FOOTER", "\033[0m\033[0m")
+TASK_RESULT_FOOTER = os.environ.get("TASK_RESULT_FOOTER", "\033[0m\033[0m") 
 
-# Pinecone namespace
-PINECONE_NAMESPACE = os.environ.get("PINECONE_NAMESPACE", "default_namespace")
+# Pinecone namespace for storing results
+PINECONE_NAMESPACE = os.environ.get("PINECONE_NAMESPACE", "default_namespace") 
 
-# Sleep time between task list checks
-SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 1))
+# Sleep time (in seconds) between task list checks
+SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 1)) 
+
+# Initial value for the task_id_counter
+TASK_ID_COUNTER = int(os.environ.get("TASK_ID_COUNTER", 0))
